71a5cd9f6341385c739b2e3f0c0800cf04fc3b2f
Saroj Karki <karki.saroj28@gmail.com>
2021-03-31 13:30:55 +0545

    Adds props to post component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 import Post from "./Post";
+import { useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "astrogeek",
+      caption: "getting into it",
+      imageUrl:
+        "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
+    },
+    {
+      username: "elon",
+      caption: "lets do it",
+      imageUrl:
+        "https://images.theconversation.com/files/10787/original/54954sbn-1337307547.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip",
+    },
+  ]);
   return (
     <div className="app">
       <div className="app__header">
@@ -11,8 +26,13 @@ function App() {
           alt=""
         />
       </div>
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./post.css";
 import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
   return (
     <div className="post">
       <div className="post__header">
@@ -11,15 +11,12 @@ function Post() {
           alt="Astrogeek"
           src="static/images/avatar/1.jpg"
         />
-        <h3>Username</h3>
+        <h3>{username}</h3>
       </div>
 
-      <img
-        className="post__image"
-        src="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-      />
+      <img className="post__image" src={imageUrl} />
       <h4 className="post__text">
-        <strong>astrogeek:</strong>lets make this
+        <strong>{username}</strong> {caption}
       </h4>
     </div>
   );
